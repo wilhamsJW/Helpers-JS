4440a42e3df1a5469ba6b497c8f6a361f58ce56a
wilhamsJW <wilhams.junior@servicenet.com.br>
2021-10-07 17:18:13 -0300

    adding pasta 11

---
+++ 9 - extraindo_itens_de_array_e_iserindo_em_outro/11 - toLocaleString()/toLocaleString().js
@@ -0,0 +1,10 @@
+function formatMoeda (item) {
+
+      // Estou dando um parseInt pq o dado no meu caso veio como string apesar de ser números
+     // então guardamos o valor convertido em uma var, caso contrário não irá fucnionar
+    // toFixed(2) -> está pegando duas casas decimais após a vírgula pq o número vem assim:
+   // "75.098839823"   
+    var itemNumber = parseInt(item).toFixed(2)
+    var formatado = itemNumber.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    return formatado;
+}
