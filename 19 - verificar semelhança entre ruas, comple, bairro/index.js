60d980f0cc53f7ccf2cc41607e50a4754647d7fd
Wilhams Junior <47488525+wilhamsJW@users.noreply.github.com>
2023-07-31 15:40:21 -0300

    Update index.js

--- 19 - verificar semelhança entre ruas, comple, bairro/index.js
+++ 19 - verificar semelhança entre ruas, comple, bairro/index.js
@@ -1,106 +1,105 @@
 function main() {
   
-  const ruaCrname  = 'Praca Benicio do Espirito Santo'
+    const ruaCrname  = 'r lenira maria dos santos reis, 353, cidade industrial, curitiba - pr, 81250720'
+  
+      let ruaClientWit = 'Rua Lenira Maria dos Santos Reis'
+      let complementOptions = 'casa' 
+      let numero = '353'
+      let similaridade = 0
+      let complement = 'casa a'
+      let zipCode = '81250720'
+      let maiorSimilaridade = 0
+      let notEnterNumber = false   
+  
+      const formatRua = (rua) => {
+        return rua.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[,\.\!\?]/g, '') || ""
+      }
     
-  //   let ruaClientWit = 'RUA FRANCISCO RODRIGUES'
-  //   let complementOptions = 'BLOCO' 
-  //   let numero = '350'
-  //   let similaridade = 0
-  //   let complement = 'Bloco 33 Apartamento 204'
-  //   let maiorSimilaridade = 0
-  //   let notEnterNumber = false    
+      // Verifica rua mais similar possivel entre rua do cliente e rua que consta no crName
+      const verificaRuaSimilar = (ruaCrname) => {
+        const palavrasRuaClientWit = formatRua(ruaClientWit).split(" ")
+        const palavrasComplementOptions = formatRua(complementOptions).split(" ")
+        const palavrasComplementAux = formatRua(complement).split(" ")
+  
+        //console.log('palavrasRuaClientWit', palavrasRuaClientWit)
+        //console.log('ruaCrname', formatRua(ruaCrname).split(' '))
     
-    let ruaClientWit = 'RUA Espirito santo'
-    let complementOptions = 'casa' 
-    let numero = '455'
-    let similaridade = 0
-    let complement = 'Bloco 13 Apartamento 678'
-    let maiorSimilaridade = 0
-    let notEnterNumber = false   
-
-    const formatRua = (rua) => {
-      return rua.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[,\.\!\?]/g, '') || ""
-    }
+        for (const palavraRua of palavrasRuaClientWit) {
+          //console.log('palavraRua Cliente:', palavraRua)
+          //console.log('ruaCrname:', formatRua(ruaCrname))
   
-    // Verifica rua mais similar possivel entre rua do cliente e rua que consta no crName
-    const verificaRuaSimilar = (ruaCrname) => {
-      const palavrasRuaClientWit = formatRua(ruaClientWit).split(" ")
-      const palavrasComplementOptions = formatRua(complementOptions).split(" ")
-      const palavrasComplementAux = formatRua(complement).split(" ")
-
-      //console.log('palavrasRuaClientWit', palavrasRuaClientWit)
-      //console.log('ruaCrname', formatRua(ruaCrname).split(' '))
-  
-      for (const palavraRua of palavrasRuaClientWit) {
-        console.log('palavraRua Cliente:', palavraRua)
-        console.log('ruaCrname:', formatRua(ruaCrname))
-
-          if (formatRua(ruaCrname).includes(palavraRua)) { // Verifica se existe palavras da rua do cliente do wit dentro da rua do crName
-            similaridade ++
-            //console.log("palavraRua igual - cliente", palavraRua)
-            console.log("1 similaridade", similaridade)
-            //console.log("ruaCrname.includes(palavraRua)", ruaCrname.includes(palavraRua))
-          }
-  
-      if (palavrasComplementOptions.length) {
-          for (const palavraComplementOp of palavrasComplementOptions) { // Verifica se existe comlementOptions dentro da rua do CrName
-            if (palavraComplementOp.trim().length === 0) { // 
-              break;
-            }
-            if (ruaCrname.includes(palavraComplementOp)) {
-              //console.log("palavraComplementOp igual", palavraComplementOp)
-              console.log(" 2 similaridade", similaridade)
+            if (formatRua(ruaCrname).includes(palavraRua)) { // Verifica se existe palavras da rua do cliente do wit dentro da rua do crName
               similaridade ++
+              //console.log("palavraRua igual - cliente", palavraRua)
+              console.log("1 similaridade - rua igual", similaridade)
+              //console.log("ruaCrname.includes(palavraRua)", ruaCrname.includes(palavraRua))
             }
-          }
-        } else if (palavrasComplementAux.length) {
-          for (const palavraComplemenAux of palavrasComplementAux) { //  Verifica de o complemento existe dentro da rua do crName
-            if (ruaCrname.includes(palavraComplemenAux)) {
-              similaridade ++
-              //console.log("palavraComplemenAux igual", palavraComplemenAux)
-              console.log("3 similaridade", similaridade)
-            }
-          }      
-        }
-  
-        if (ruaCrname.includes(numero) && !notEnterNumber) { // verificando se o numero da rua do cliente tem na rua do crName
-          similaridade++;
-          notEnterNumber = true
-          //console.log('numero igual')
-          console.log(" 4 similaridade", similaridade)
-        }    
-  
-        if (similaridade > maiorSimilaridade) {
-          maiorSimilaridade = similaridade
-        }
-  
-      } // end for main
-    }
-
-    verificaRuaSimilar(ruaCrname)
     
-    if (maiorSimilaridade <= 1) {
-        //console.log('maiorSimilaridade', maiorSimilaridade)
-        //return 'NOVA VENDA'
-    }
+        if (palavrasComplementOptions.length) {
+            for (const palavraComplementOp of palavrasComplementOptions) { // Verifica se existe comlementOptions dentro da rua do CrName
+              if (palavraComplementOp.trim().length === 0) {
+                break;
+              }
+              if (ruaCrname.includes(palavraComplementOp)) {
+                //console.log("palavraComplementOp igual", palavraComplementOp)
+                console.log(" 2 similaridade - palavraComplementOp igual", similaridade)
+                similaridade ++
+              }
+            }
+          } else if (palavrasComplementAux.length) {
+            for (const palavraComplemenAux of palavrasComplementAux) { //  Verifica de o complemento existe dentro da rua do crName
+              if (ruaCrname.includes(palavraComplemenAux)) {
+                similaridade ++
+                //console.log("palavraComplemenAux igual", palavraComplemenAux)
+                console.log("3 similaridade - palavraComplemenAux igual", similaridade)
+              }
+            }      
+          }
+    
+          if (ruaCrname.includes(numero) && !notEnterNumber) { // verificando se o numero da rua do cliente tem na rua do crName
+            similaridade++;
+            notEnterNumber = true
+            //console.log('numero igual')
+            console.log(" 4 similaridade - numero igual", similaridade)
+          }    
 
-    const ruaCrnameLength = ruaCrname.split(' ').length
-    //const proximidade = (Math.abs(maiorSimilaridade - ruaCrnameLength) / maiorSimilaridade) * 10
-    const diferencaEntreRua = Math.abs(maiorSimilaridade - ruaCrnameLength)
-    const porcentagemDistancia = (diferencaEntreRua / Math.max(maiorSimilaridade, ruaCrnameLength)) * 100;
-
-
-    if (porcentagemDistancia < 40) {
-      console.log('clikooooooooooo neleeeeeee')
-    }
-
-
-    return {
-      lengthRuaCrName: ruaCrnameLength,
-      palavrasParecidasEntreAsRuas: maiorSimilaridade,
-      diferencaEntreRuas: diferencaEntreRua,
-      porcentagemDistancia: porcentagemDistancia.toFixed(0) == '0' ? 'Exatamente iguais' : porcentagemDistancia.toFixed(0)
-    };
-}
+          if (ruaCrname.includes(zipCode)) {
+            similaridade++;
+            console.log(" 5 similaridade - zipcode igual", similaridade)
+          }
+    
+          if (similaridade > maiorSimilaridade) {
+            maiorSimilaridade = similaridade
+          }
+    
+        } // end for main
+      }
+  
+      verificaRuaSimilar(ruaCrname)
       
-  console.log('main():', main())
+      if (maiorSimilaridade <= 1) {
+          //console.log('maiorSimilaridade', maiorSimilaridade)
+          //return 'NOVA VENDA'
+      }
+  
+      const ruaCrnameLength = ruaCrname.split(' ').length
+      //const proximidade = (Math.abs(maiorSimilaridade - ruaCrnameLength) / maiorSimilaridade) * 10
+      const diferencaEntreRua = Math.abs(maiorSimilaridade - ruaCrnameLength)
+      const porcentagemDistanciaProximidade = (diferencaEntreRua / Math.max(maiorSimilaridade, ruaCrnameLength)) * 100;
+  
+      // Se for menor que 40 indica que esta é a rua igual
+  
+      if (porcentagemDistanciaProximidade < 40) {
+        console.log('cliko nele ou seleciono ele')
+      }
+  
+  
+      return {
+        lengthRuaCrName: ruaCrnameLength,
+        palavrasParecidasEntreAsRuas: maiorSimilaridade,
+        diferencaEntreRuas: diferencaEntreRua,
+        porcentagemDistanciaProximidade: porcentagemDistanciaProximidade.toFixed(0) == '0' ? 'Exatamente iguais' : porcentagemDistanciaProximidade.toFixed(0)
+      };
+  }
+        
+    console.log('main():', main())
